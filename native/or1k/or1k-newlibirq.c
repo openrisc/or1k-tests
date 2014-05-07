a5ebc2c1802695a0e5b8e0980deeab67f84c4d4f
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2014-05-07 09:04:47 +0300

    add 'native' testsuite
    The meaning of 'native' in this context should be interpreted
    as 'tests defined within or1k-tests' (opposed to tests
    pulled in from external sources).
    
    The testsuite from mor1kx-devenv is added here, with a
    proof-of-concept Makefile that allows it to be run standalone.

---
+++ native/or1k/or1k-newlibirq.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <spr-defs.h>
+#include <or1k-support.h>
+//#include "board.h"
+#define INTGEN_BASE         0xe1000000
+#define INTGEN_IRQ                  19
+
+extern void or1k_interrupt_handler(void);
+
+static void write_intgen_reg(uint32_t reg, int value)
+{
+	*((uint8_t *)INTGEN_BASE + reg) = value;
+}
+
+void intgen_isr(void *data)
+{
+	/* Write 0 to address 1 clears interrupt */
+	write_intgen_reg(1, 0);
+	printf("got irq\r\n");
+}
+
+int main(void)
+{
+	/* Install interrupt handler and enable interrupt */
+	or1k_exception_handler_add(0x8, or1k_interrupt_handler);
+	or1k_interrupt_handler_add(INTGEN_IRQ, intgen_isr);
+	or1k_interrupt_enable(INTGEN_IRQ);
+	or1k_mtspr(SPR_SR, or1k_mfspr(SPR_SR) | SPR_SR_IEE);
+	/* Generate an interrupt */
+	write_intgen_reg(0, 1);
+
+	/* Exit simulation */
+	asm("l.nop 0x1");
+
+	return 0;
+}
