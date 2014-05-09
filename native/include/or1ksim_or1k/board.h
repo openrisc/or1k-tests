3088c60011baf52c3076f756aca48dc6188086c7
Peter Gavin <pgavin@gmail.com>
2014-05-09 08:43:48 -0400

    add support for or1ksim

---
+++ native/include/or1ksim_or1k/board.h
@@ -0,0 +1,26 @@
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#define IN_CLK  	      50000000 // Hz
+
+
+//
+// Defines for each core (memory map base, OR1200 interrupt line number, etc.)
+//
+#define RAM_BASE            0x00000000
+
+#define UART0_BASE  	    0x90000000
+#define UART0_IRQ                    2
+#define UART0_BAUD_RATE 	115200
+
+#define SPI0_BASE           0xb0000000
+
+
+//
+// OR1200 tick timer period define
+//
+#define TICKS_PER_SEC   100
+
+
+
+#endif
